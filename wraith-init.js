41a1179f9a07719981719ffc3d62a0df1a31a44f
Adam Simpson <adam@heysparkbox.com>
2015-05-01 11:26:52 -0400

    feat: first stab at v1

---
+++ wraith-init.js
@@ -0,0 +1,17 @@
+var shelljs = require('shelljs'),
+    Slack = require('slack-client'),
+    config = require('./config.json');
+    autoReconnect = true,
+    autoMark = true;
+
+if (shelljs.exec('bundle exec wraith capture wraith/wraith.yaml').code !== 0) {
+  var slack = new Slack(config.slackToken, autoReconnect, autoMark);
+
+  slack.on("open", function() {
+    var channel = slack.getChannelGroupOrDMByID(config.slackChannel);
+    channel.send('There are significant visual changes, please review - ' + config.divshotURL);
+    shelljs.exit(0);
+  });
+
+  slack.login();
+}
